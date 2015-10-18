cc4cdb574ca65317c209e323f746a737b0653a8f
Azure8705 <kvnliwag@gmail.com>
2015-10-18 14:26:34 -0700

    Initial Commit

---
+++ server.js
@@ -0,0 +1,56 @@
+var unirest = require('unirest');
+var express = require('express');
+var events = require('events');
+
+var app = express();
+app.use(express.static('public'));
+
+var getFromApi = function(endpoint, args) {
+    var emitter = new events.EventEmitter();
+    unirest.get('https://api.spotify.com/v1/' + endpoint )
+           .qs(args)
+           .end(function(response) {
+                if (response.ok) {
+                    emitter.emit('end', response.body);
+                }
+                else {
+                    emitter.emit('error', response.code);
+                }
+            });
+    return emitter;
+};
+
+var getRelatedArtists = function(res, artist){
+    var relatedUrl = 'artists/' + artist.id + '/related-artists';
+    var relatedReq = getFromApi(relatedUrl, {});
+
+    relatedReq.on('end', function(relatedArtists){
+        artist.related = relatedArtists.artists;
+        res.json(artist);
+    });
+
+    relatedReq.on('error', function() {
+        res.statusCode = 404;
+        res.end();
+    });
+};
+
+app.get('/search/:name', function(req, res) {
+    var searchReq = getFromApi('search', {
+        q: req.params.name,
+        limit: 1,
+        type: 'artist'
+    });
+
+    searchReq.on('end', function(item) {
+        var artist = item.artists.items[0];
+        getRelatedArtists(res, artist);
+                
+    });
+
+    searchReq.on('error', function(code) {
+        res.sendStatus(code);
+    });
+});
+
+app.listen(8080);
